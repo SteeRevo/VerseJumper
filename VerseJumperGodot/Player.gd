eb5c2edfbb1fc510625de16d3ca8c38d7827cec8
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2022-09-30 22:27:31 -0700

    added player movement and out of bounds

--- VerseJumperGodot/Player.gd
+++ VerseJumperGodot/Player.gd
@@ -5,6 +5,14 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+const ACCELERATION = 300
+const MAX_SPEED = 300
+const FRICTION = 300
+
+var motion = Vector2.ZERO
+
+onready var sprite = $Sprite
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +20,21 @@ func _ready():
 
 
 func _physics_process(delta):
-	pass
+	#moves player based on input strength
+	var inputVector = Vector2.ZERO
+	inputVector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	inputVector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	
+	
+	
+	#moves at rate of acceleration times delta and maxes out at max speed
+	if inputVector != Vector2.ZERO:
+		motion += inputVector * ACCELERATION * delta
+		motion = motion.clamped(MAX_SPEED)
+	else:
+		motion = motion.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	
+	#performs movement of player
+	motion = move_and_slide(motion, Vector2.UP);
